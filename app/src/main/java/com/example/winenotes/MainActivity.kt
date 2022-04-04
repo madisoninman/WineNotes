90c7d35ab13a6c89e000208ec0ad1f295377e3fb
Madison Inman <madisonginman@gmail.com>
2022-04-04 16:59:21 -0500

    added adapter and view holder - added binding to Note

--- app/src/main/java/com/example/winenotes/MainActivity.kt
+++ app/src/main/java/com/example/winenotes/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.example.winenotes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import android.view.*
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.RecyclerView
 import com.example.winenotes.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: MyAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,5 +30,42 @@ class MainActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return super.onOptionsItemSelected(item)
+    }
+
+
+    inner class MyViewHolder(val view: TextView) :
+        RecyclerView.ViewHolder(view),
+        View.OnClickListener, View.OnLongClickListener {
+
+        init {
+            view.setOnClickListener(this)
+            view.setOnLongClickListener(this)
+        }
+
+        override fun onClick(view: View?) {
+
+        }
+
+        override fun onLongClick(view: View?): Boolean {
+            return true
+        }
+    }
+
+    inner class MyAdapter :
+        RecyclerView.Adapter<MyViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_view, parent, false) as TextView
+            return MyViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+        }
+
+        override fun getItemCount(): Int {
+            return 0
+        }
     }
 }
--- app/src/main/java/com/example/winenotes/NoteActivity.kt
+++ app/src/main/java/com/example/winenotes/NoteActivity.kt
@@ -1,6 +1,19 @@
 package com.example.winenotes
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.winenotes.databinding.ActivityNoteBinding
 
 class NoteActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityNoteBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityNoteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+    }
 }

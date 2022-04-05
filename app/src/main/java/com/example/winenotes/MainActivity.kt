f21730fe74089e998ca2a9530b620dc1d7c34cfa
Madison Inman <madisonginman@gmail.com>
2022-04-05 13:34:34 -0500

    setup intent for Add Note

--- app/src/main/java/com/example/winenotes/MainActivity.kt
+++ app/src/main/java/com/example/winenotes/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.winenotes
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.*
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -61,7 +64,38 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return super.onOptionsItemSelected(item)
+        when (item.getItemId()) {
+            R.id.menu_item_add -> {
+                addNewNote()
+                return true
+            }
+            R.id.title_sort_menu_item -> {
+                return true
+            }
+            R.id.modified_sort_menu_item -> {
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    private val startForAddResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        result:ActivityResult ->
+
+        if (result.resultCode == Activity.RESULT_OK) {
+
+        }
+
+    }
+
+    private fun addNewNote() {
+        val intent = Intent(applicationContext, NoteActivity::class.java)
+        intent.putExtra(
+            getString(R.string.intent_purpose_key),
+            getString(R.string.intent_purpose_add_note)
+        )
+        startForAddResult.launch(intent)
     }
 
 
--- app/src/main/java/com/example/winenotes/NoteActivity.kt
+++ app/src/main/java/com/example/winenotes/NoteActivity.kt
@@ -1,16 +1,23 @@
 package com.example.winenotes
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.winenotes.databinding.ActivityNoteBinding
 
 class NoteActivity : AppCompatActivity() {
     private lateinit var binding: ActivityNoteBinding
+    private var purpose : String? = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityNoteBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val intent = getIntent()
+        purpose = intent.getStringExtra(getString(R.string.intent_purpose_key))
+
+        setTitle("$purpose Note")
     }
 
     override fun onBackPressed() {
